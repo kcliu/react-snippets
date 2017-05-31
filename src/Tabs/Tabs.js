2592f7ef11ecedd0b1ae89a3d88f3c4554b9a7bc
kcliu <gjliou@cs.nctu.edu.tw>
2017-05-31 22:05:59 +0800

    refactor tabs component

--- src/Tabs/Tabs.js
+++ src/Tabs/Tabs.js
@@ -4,25 +4,15 @@ import classnames from 'classnames';
 import './Tabs.css';
 
 const Tabs = ({ currentIndex, onClick, sourceName }) => {
-  const sourceCx = (clicked) => {
-    return classnames('tab', {
-      active: clicked === currentIndex,
-    })
-  }
-
-  const renderTab = () => {
-    return sourceName.map((d, i) => {
-      return (
-        <div className={sourceCx(i)} onClick={onClick(i)} key={i}>
-          {d}
-        </div>
-      )
-    })
-  }
-
   return (
     <div className="tab-wrapper">
-      {renderTab()}
+      {
+        sourceName.map((d, i) =>
+          <div className={classnames('tab', {
+            active: i === currentIndex
+          })} onClick={onClick(i)} key={i}>{d}</div>
+        )
+      }
     </div>
   );
 };
